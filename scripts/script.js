f6d57281f3cb0b3244c4cd3b6f1fcfdb9d6c6f3c
Lucas Regis de Moraes <LucasGRM03@gmail.com>
2022-08-28 15:34:36 -0300

    add dark theme

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Dark Theme 
+document.getElementById("dark-theme-button").addEventListener("click", ()=> document.querySelector("html").classList.toggle("dark-theme-active"))
+// ou
+/*
+document.getElementById("dark-theme-button").addEventListener("click", ()=> {
+    document.querySelector("html").classList.toggle("dark-theme-active")
+})
+*/
+
+
